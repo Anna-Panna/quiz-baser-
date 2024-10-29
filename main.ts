68c4d4a664b4c5a3dc3a341eda1caed652a6e860
Anna-Panna <anna.m.neumeier@icloud.com>
2024-10-29 10:07:41 +0100

    Eine Farbe hinzugefügt

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 radio.setGroup(98)
 basic.forever(function () {
+    basic.setLedColors(0x00ff00, 0x00ff00, 0x00ff00)
     if (input.buttonIsPressed(Button.A)) {
         radio.sendValue("A", 1)
     } else if (input.buttonIsPressed(Button.B)) {
